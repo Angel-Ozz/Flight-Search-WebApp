4c9c39a88e699917904a1e769a2ba804696b2801
Angel Ocariz Martinez <angelocariz@ENCEDCCUULT0954.local>
2025-01-30 13:45:35 -0600

    security issues

--- api/src/main/java/com/flightsearch/api/ApiApplication.java
+++ api/src/main/java/com/flightsearch/api/ApiApplication.java
@@ -3,10 +3,18 @@ package com.flightsearch.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+			System.setProperty("AMADEUS_API_KEY", dotenv.get("AMADEUS_API_KEY"));
+			System.setProperty("AMADEUS_API_SECRET", dotenv.get("AMADEUS_API_SECRET"));
+			System.setProperty("AMADEUS_GRANT_TYPE", dotenv.get("AMADEUS_GRANT_TYPE"));
+			System.setProperty("AMADEUS_TOKEN_URI", dotenv.get("AMADEUS_TOKEN_URI"));
+
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
